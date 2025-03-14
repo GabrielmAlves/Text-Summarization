35dbd19aefe16416fce41478e089cbbf64e98eab
GabrielmAlves <gabrielmalves10@gmail.com>
2025-03-13 21:06:42 -0300

    remove stopwords and punctuation from the user text

--- src/process_text.py
+++ src/process_text.py
@@ -8,6 +8,16 @@ nlp = spacy.load('pt_core_news_lg')
 
 def process_text(text):
     doc = nlp(text)
+
+    important_words = []
+    stopwords = list(STOP_WORDS)
+    pos_tag_list = ['ADJ', 'PROPN', 'NOUN', 'VERB']
     
-    print(f"Number of sentences in {text}: ")
-    len(list(doc.sents))
+    for token in doc:
+        if (token.text in stopwords or token.text in punctuation):
+            continue
+        if (token.pos_ in pos_tag_list):
+            important_words.append(token.text)
+            
+    for t in important_words:
+        print(t)
